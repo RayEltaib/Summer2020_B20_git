774a45335b062c88340025a98683ef7a1fd20a7b
Rayman <abdo.eltaib.1970@gmail.com>
2020-08-05 21:33:31 -0400

    Update SomeCodes.java

--- src/utility/SomeCodes.java
+++ src/utility/SomeCodes.java
@@ -3,5 +3,6 @@ package utility;
 public class SomeCodes {
     public static void main(String[] args) {
         System.out.println("Take some time off");
+        System.out.println("Can you see this line");
     }
 }
